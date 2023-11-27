0830de347cba79d252f40bdca3848f406954d0a6
Pedro Germano <pedrogermano232@gmail.com>
2023-11-27 13:00:24 -0300

    vilian

--- js/main.js
+++ js/main.js
@@ -5,5 +5,9 @@ let swiper = new Swiper(".slide-video", {
   pagination: {
     el: ".s-videos .swiper-pagination",
     clickable: true,
-  }
-})
+  },
+  navigation: {
+    nextEl: ".s-videos .btn-next",
+    prevEl: ".s-videos .btn-prev"
+  },
+});
